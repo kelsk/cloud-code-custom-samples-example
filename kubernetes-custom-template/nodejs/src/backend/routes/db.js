b18edf19ca1d83f4873827e518fa5734e58c5743
kelsk <38271546+kelsk@users.noreply.github.com>
2021-04-15 17:31:41 -0700

    Revert "Merge "kelsk-rename" into "kelsk-subtrees""

--- kubernetes-custom-template/nodejs/src/backend/routes/db.js
+++ kubernetes-custom-template/nodejs/src/backend/routes/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const SAMPLE_DB_ADDR = process.env.SAMPLE_DB_ADDR; 
-const mongoURI = "mongodb://" + SAMPLE_DB_ADDR + "/sample"
+const TEMPLATE_DB_ADDR = process.env.TEMPLATE_DB_ADDR; 
+const mongoURI = "mongodb://" + TEMPLATE_DB_ADDR + "/template"
 
 const db = mongoose.connection;
 db.on('disconnected', () => {
